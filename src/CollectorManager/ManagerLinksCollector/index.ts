fef621d96d9b732d095652fbc69311cec1d89821
kenriquelibgot <kenrique@libgot.com>
2025-01-28 14:00:33 -0300

    Seasson 2 fix error send embeds

--- src/CollectorManager/ManagerLinksCollector/index.ts
+++ src/CollectorManager/ManagerLinksCollector/index.ts
@@ -95,7 +95,7 @@ export const LinksButtons = async (interaction: ButtonInteraction<CacheType>, cl
               const embeds = await SendEndMissionEmbedWithPoints(role) as EmbedBuilder[]
               const data = ButtonData(interaction.user.id, role)
               const components = interaction.customId.split('-')[1] === 'detector' ? await CreateLinkDetectorButtons(data) as ActionRowBuilder<ButtonBuilder>[] : await CreateSettingButtonOptions(data[0]) as ActionRowBuilder<ButtonBuilder>[]
-              interaction.update({ embeds, components }) 
+              interaction.reply({ embeds, components }) 
             }
           })
         .catch((err) => console.log(err))
